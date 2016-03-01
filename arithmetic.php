7666b01e18daefc9e3a3675d7719e17e0261807a
Michael Burney <burney318@gmail.com>
2016-03-01 15:19:56 -0600

    Finished arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -2,29 +2,30 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+    return $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b)
 {
-	return $a - $b;
+	return $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b)
 {
-	return $a * $b;	
+	return $a * $b . PHP_EOL;	
 }
 
 function divide($a, $b)
 {
-	return $a / $b;	
+	return $a / $b . PHP_EOL;	
 }
 function modulus($a, $b){
-	return $a % $b;
+	return $a % $b . PHP_EOL;
 }
 
 
-add(1,3);
-subtract(2,5);
-multiply(2,3);
-divide(10,4);
+echo add(1,3);
+echo subtract(2,5);
+echo multiply(2,3);
+echo divide(10,4);
+echo modulus(4,7);
