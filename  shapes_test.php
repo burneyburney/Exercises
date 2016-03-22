5a8b8688bf21b1e272e6fd1177e4ceec731380b3
michael burney <michaelburney@michaels-MacBook-Pro.local>
2016-03-22 14:03:52 -0500

    creation of shapes/rectangle to test classes parent/child

---
+++  shapes_test.php
@@ -0,0 +1,19 @@
+<!-- shapes_test.php -->
+<!-- 9.2.1 -->
+<?php
+require_once 'rectangle.php';
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>shapes_test.php</title>
+  </head>
+  <body>
+    <h1> shapes_test.php | 9.2.1</h1>
+  </body>
+</html>
