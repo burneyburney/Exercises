ba9dac07e3b4f4656cd87b7f86ad17f32368ce25
michael burney <michaelburney@michaels-MacBook-Pro.local>
2016-03-23 15:36:00 -0500

    Exercise 9.2.4 Magic Methods (get/set)

---
+++ model.php
@@ -0,0 +1,40 @@
+<?php
+//  Michael Burney
+//  model.php
+//  9.2.4  
+
+class Data
+{
+    // empty array
+    private $data = [];
+
+    // populate the data array, magic setter
+    public function __set($name, $value)
+    {
+        // Set the $name key to hold $value in $data
+        $this->data[$name] = $value;
+    }
+
+    // Magic getter to retrieve values from $data
+    public function __get($name)
+    {
+        // Check for existence of array key $name
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+        // if it gets here return NULL
+        return null;
+    }
+}
+
+// THIS IS A OBJECT MOFO!
+$poop = new Data();
+// we gave it some stuff
+$poop->crap = 'notcrap';
+$poop->dump = 'notdump';
+
+// now we wanna see it on the screen
+var_dump($poop);
+
+
+?>
