36bc7ac7f82bef5175320304f3954fc0f77331f6
michael burney <burney318@gmail.com>
2016-03-29 13:37:56 -0500

    addition of model_test

---
+++ model_test.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . '/User.php';
+
+// Create a new user using the User model.
+abstract class newUser extends User
+
+// Find the new user by its id.
+if(array_key_exists(key, array)){
+
+}
+
+// Update the found user to have new values.
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+// Verify the update executed in the DB.
+if(array_key_exists(1,array){
+    echo 'you have a key!'
+} else{
+    echo 'no key bro!'
+}
+
+// Create another new user.
+$anotherNewUser = new User
+
+// Retrieve all users from the DB.
+SELECT * FROM sys.database_principals
+
+// Add a method to delete a record in the Model class.
+
+// Delete a user using the new method added to the base class.
+
+public static function
